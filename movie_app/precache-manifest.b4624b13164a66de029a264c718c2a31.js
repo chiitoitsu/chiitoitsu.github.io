a40752a34338b4eec183949d43412ed70d5e7680
chiitoitsu <wanwan@tong.dongyang.ac.kr>
2019-12-29 19:40:20 +0900

    react app

---
+++ movie_app/precache-manifest.b4624b13164a66de029a264c718c2a31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dce918bf7cc14d24be6b085ff3522bea",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "e06eaba61ec355ea43ef",
+    "url": "/movie_app/static/css/main.048f81f1.chunk.css"
+  },
+  {
+    "revision": "a2d122b13929b0048ec6",
+    "url": "/movie_app/static/js/2.68f05489.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/movie_app/static/js/2.68f05489.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e06eaba61ec355ea43ef",
+    "url": "/movie_app/static/js/main.4fc318c6.chunk.js"
+  },
+  {
+    "revision": "3dd3f4ef526798ea7005",
+    "url": "/movie_app/static/js/runtime-main.ec52c1d1.js"
+  }
+]);
