abe319741b8d1232ea0cc99fc86257790c713222
chiitoitsu <wanwan@tong.dongyang.ac.kr>
2020-01-12 21:04:02 +0900

    react portfolio

---
+++ portfolio/precache-manifest.5b7e8f617e1ccfc00875b5b4be330b61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7adc9151231af824aa2e238555b7be47",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "7d479f2fca5c6beccf18",
+    "url": "/portfolio/static/css/main.a1f81557.chunk.css"
+  },
+  {
+    "revision": "d24437d3a4b23ae0b65d",
+    "url": "/portfolio/static/js/2.e9d9d749.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/portfolio/static/js/2.e9d9d749.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7d479f2fca5c6beccf18",
+    "url": "/portfolio/static/js/main.2964aa96.chunk.js"
+  },
+  {
+    "revision": "2f2de9561c9456cd96c1",
+    "url": "/portfolio/static/js/runtime-main.5b94bbb3.js"
+  }
+]);
