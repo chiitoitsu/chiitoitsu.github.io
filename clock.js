c77630ac21c86f807f6e491a24fe16f6af267e86
chiitoitsu <wanwan@tong.dongyang.ac.kr>
2020-01-23 22:40:26 +0900

    Add temp index

---
+++ clock.js
@@ -0,0 +1,20 @@
+const clockContainer = document.querySelector('.js-clock')
+const clockTitle = clockContainer.querySelector('h1')
+
+function getTime() {
+	const date = new Date()
+	const hours = date.getHours()
+	const mins = date.getMinutes()
+	const secs = date.getSeconds()
+	clockTitle.innerText =
+		`${hours < 10 ? `0${hours}` : `${hours}`}:` +
+		`${mins < 10 ? `0${mins}` : `${mins}`}:` +
+		`${secs < 10 ? `0${secs}` : `${secs}`}`
+}
+
+function init() {
+	getTime()
+	setInterval(getTime, 1000)
+}
+
+init()
