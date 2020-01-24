7641236858c31d249ce0b7c67deda8691ea1b3dc
chiitoitsu <50188166+chiitoitsu@users.noreply.github.com>
2020-01-24 15:54:02 +0900

    Update clock.js
    add 12 & 24 hours mode

--- clock.js
+++ clock.js
@@ -1,15 +1,32 @@
 const clockContainer = document.querySelector('.js-clock')
 const clockTitle = clockContainer.querySelector('h1')
+const clockButton = clockContainer.querySelector('#js-btn')
+let is_clock24 = false
 
 function getTime() {
 	const date = new Date()
 	const hours = date.getHours()
 	const mins = date.getMinutes()
 	const secs = date.getSeconds()
-	clockTitle.innerText =
-		`${hours < 10 ? `0${hours}` : `${hours}`}:` +
-		`${mins < 10 ? `0${mins}` : `${mins}`}:` +
-		`${secs < 10 ? `0${secs}` : `${secs}`}`
+
+	if (is_clock24) {
+		clockTitle.innerText =
+			`${hours < 10 ? `0${hours}` : `${hours}`}:` +
+			`${mins < 10 ? `0${mins}` : `${mins}`}:` +
+			`${secs < 10 ? `0${secs}` : `${secs}`}`
+	} else {
+		clockTitle.innerText =
+			`${hours < 12 ? `오전 ` : `오후 `}` +
+			`${hours < 10 ? `0${hours % 12}` : `${hours % 12}`}:` +
+			`${mins < 10 ? `0${mins}` : `${mins}`}:` +
+			`${secs < 10 ? `0${secs}` : `${secs}`}`
+	}
+}
+
+function selectTime() {
+	is_clock24 = !is_clock24
+	clockButton.innerText = `${is_clock24 ? `12시간 표기` : `24시간 표기`}`
+	getTime()
 }
 
 function init() {
